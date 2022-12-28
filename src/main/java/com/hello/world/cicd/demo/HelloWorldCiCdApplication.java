c87550f8785ecaf352192cd67825258de3e2821d
Javier Chiquito Avellán <wchiquito@eclipsoft.com>
2022-12-28 17:18:20 -0500

    Initialize application

---
+++ src/main/java/com/hello/world/cicd/demo/HelloWorldCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.hello.world.cicd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldCiCdApplication.class, args);
+	}
+
+}
