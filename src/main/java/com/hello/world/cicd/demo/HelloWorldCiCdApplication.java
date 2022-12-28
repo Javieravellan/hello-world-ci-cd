fa5d69f4dd7d7025204f954c6e9505d6fedf932f
Javier Chiquito Avellán <wchiquito@eclipsoft.com>
2022-12-28 17:55:12 -0500

    add api endpoint

--- src/main/java/com/hello/world/cicd/demo/HelloWorldCiCdApplication.java
+++ src/main/java/com/hello/world/cicd/demo/HelloWorldCiCdApplication.java
@@ -2,12 +2,20 @@ package com.hello.world.cicd.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/api")
 public class HelloWorldCiCdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldCiCdApplication.class, args);
 	}
 
+	@GetMapping("/saludos/{name}")
+	public String saludar(@PathVariable String name) {
+		return "Hello, " + name;
+	}
 }
